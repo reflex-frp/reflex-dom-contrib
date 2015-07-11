4b99598c870835e2163b470f34ea792e005052f9
Doug Beardsley <mightybyte@gmail.com>
2015-07-11 17:41:55 -0400

    Add checkboxList widget

---
+++ src/Reflex/Dom/Contrib/Widgets/CheckboxList.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE ScopedTypeVariables      #-}
+
+module Reflex.Dom.Contrib.Widgets.CheckboxList where
+
+------------------------------------------------------------------------------
+import           Control.Monad
+import           Data.Set (Set)
+import qualified Data.Set as S
+import           Reflex
+import           Reflex.Dom
+import           Reflex.Dom.Contrib.Widgets.Common
+------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------
+-- | Takes a list of labels to make checkboxes for and returns the labels of
+-- the boxes that are checked.
+checkboxList
+    :: forall t m a. (MonadWidget t m, Ord a, Show a)
+    => (a -> String)
+    -- ^ Function to show each item
+    -> (String -> a -> Bool)
+    -- ^ Function to filter each item
+    -> Event t Bool
+    -- ^ Blanket event to apply to all list items.  Allows you to have "select
+    -- all" and "select none" buttons.  Fire True to select all and False to
+    -- select none.
+    -> Dynamic t String
+    -- ^ A search string for filtering the list of items.
+    -> Set a
+    -- ^ Set of items that should be initially checked
+    -> [a]
+    -- ^ List of items to show checkboxes for
+    -> m (HtmlWidget t [a])
+    -- ^ Dynamic list of checked items
+checkboxList showFunc filterFunc blanketEvent searchString onItems items = do
+    el "ul" $ do
+      es <- forM items $ \item -> do
+        let shown = showFunc item
+            mkAttrs search =
+              if filterFunc search item
+                then mempty
+                else "style" =: "display:none"
+        attrs <- liftM nubDyn $ mapDyn mkAttrs searchString
+        elDynAttr "li" attrs $ el "label" $ do
+          cb <- htmlCheckbox $ WidgetConfig
+                  (leftmost [blanketEvent])
+                  (S.member item onItems)
+                  (constDyn mempty)
+          text shown
+          mapWidget (\b -> if b then [item] else []) cb
+      wconcat es
+
