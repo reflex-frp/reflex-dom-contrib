92a75bc6990154239a87c6f190fc988989bdef65
Doug Beardsley <mightybyte@gmail.com>
2015-08-14 18:20:49 -0400

    Add checkboxListView

--- src/Reflex/Dom/Contrib/Widgets/CheckboxList.hs
+++ src/Reflex/Dom/Contrib/Widgets/CheckboxList.hs
@@ -51,3 +51,45 @@ checkboxList showFunc filterFunc blanketEvent searchString onItems items = do
           mapWidget (\b -> if b then [item] else []) cb
       wconcat es
 
+
+------------------------------------------------------------------------------
+-- | Takes a list of labels to make checkboxes for and returns the labels of
+-- the boxes that are checked.
+checkboxListView
+    :: forall t m a b. (MonadWidget t m, Ord a, Show a)
+    => (a -> String)
+    -- ^ Function to show each item
+    -> (String -> a -> Bool)
+    -- ^ Function to filter each item
+    -> (a -> Bool -> b)
+    -> Event t Bool
+    -- ^ Blanket event to apply to all list items.  Allows you to have "select
+    -- all" and "select none" buttons.  Fire True to select all and False to
+    -- select none.
+    -> Dynamic t String
+    -- ^ A search string for filtering the list of items.
+    -> Set a
+    -- ^ Set of items that should be initially checked
+    -> [a]
+    -- ^ List of items to show checkboxes for
+    -> m (Event t b)
+    -- ^ Events changing the selected set
+checkboxListView showFunc filterFunc updateFunc blanketEvent searchString
+                 onItems items = do
+    el "ul" $ do
+      es <- forM items $ \item -> do
+        let shown = showFunc item
+            mkAttrs search =
+              if filterFunc search item
+                then mempty
+                else "style" =: "display:none"
+        attrs <- liftM nubDyn $ mapDyn mkAttrs searchString
+        elDynAttr "li" attrs $ el "label" $ do
+          cb <- htmlCheckbox $ WidgetConfig
+                  (leftmost [blanketEvent])
+                  (S.member item onItems)
+                  (constDyn mempty)
+          text shown
+          return $ updateFunc item <$> _hwidget_change cb
+      return $ leftmost es
+
