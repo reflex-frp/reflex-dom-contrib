91b88507052803cb64b03a913c335ae70501198e
Greg Hale <imalsogreg@gmail.com>
2015-12-13 23:51:00 -0500

    Better input/output handling in button group and bootstrap button group

--- src/Reflex/Dom/Contrib/Widgets/ButtonGroup.hs
+++ src/Reflex/Dom/Contrib/Widgets/ButtonGroup.hs
@@ -6,8 +6,10 @@
 
 module Reflex.Dom.Contrib.Widgets.ButtonGroup where
 
+import Data.Bool
 import Data.Map
 import qualified Data.Map as Map
+import Data.Maybe
 import Data.Monoid
 import Reflex.Dom
 import Reflex.Dom.Contrib.Widgets.Common
@@ -15,21 +17,37 @@ import Reflex.Dom.Contrib.Widgets.Common
 
 ------------------------------------------------------------------------------
 buttonGroup'
-  :: forall t m a.MonadWidget t m
+  :: forall t m a.(MonadWidget t m, Eq a, Show a)
   => (Maybe Int -> Dynamic t a -> Dynamic t Bool -> m (Event t ()))
   -> Dynamic t (Map Int a)
   -> GWidget t m (Maybe a)
-buttonGroup' drawDynBtn dynButtons cfg =
+buttonGroup' drawDynBtn dynButtons (WidgetConfig wcSet wcInit wcAttrs) =
 
-  elDynAttr "div" (_widgetConfig_attributes cfg) $ mdo
+  elDynAttr "div" wcAttrs $ mdo
 
-    dynK :: Dynamic t (Maybe Int) <- holdDyn Nothing $ selEvents
+    pb <- getPostBuild
+
+    let externalSet :: Event t (Maybe Int)
+        externalSet = attachWith revLookup (current dynButtons) wcSet
+
+    let initSet :: Event t (Maybe Int)
+        initSet = attachWith revLookup (current dynButtons) (traceEvent "INIT" $ wcInit <$ pb)
+
+    let internalSet :: Event t (Maybe Int)
+        internalSet = leftmost [initSet, clickSelEvents]
+
+        internalV :: Event t (Maybe a)
+        internalV = attachWith (\m k -> k >>= flip Map.lookup m)
+                               (current dynButtons)
+                               internalSet
+
+    dynK :: Dynamic t (Maybe Int) <- holdDyn Nothing $ leftmost [internalSet, externalSet]
 
     dynButtons' :: Dynamic t (Map (Maybe Int) a) <- mapDyn
                                                     (mapKeys Just)
                                                     dynButtons
 
-    selEvents :: Event t (Maybe Int) <- selectViewListWithKey_
+    clickSelEvents :: Event t (Maybe Int) <- selectViewListWithKey_
                                         dynK
                                         dynButtons'
                                         drawDynBtn
@@ -38,32 +56,63 @@ buttonGroup' drawDynBtn dynButtons cfg =
     dynSelV :: Dynamic t (Maybe a) <- combineDyn
                                       (\k m -> k >>= flip Map.lookup m)
                                       dynK dynButtons
-    return (HtmlWidget dynSelV undefined never never never (constDyn False))
-
-
-
+    --      HTMLWidget _hwidget_value _hwidget_change _hwidget_keypress _hwidget_keydown _hwidget_keyup _hwidget_hasfocus
+    return (HtmlWidget dynSelV (traceEvent "internalV: " internalV) never never never (constDyn False))
 
 
 ------------------------------------------------------------------------------
-bootstrapButtonGroup :: MonadWidget t m => Dynamic t [(String,a)] -> GWidget t m (Maybe a)
+revLookup :: Eq a => Map Int a -> Maybe a -> Maybe Int
+revLookup _ Nothing  = Nothing
+revLookup m (Just v) = listToMaybe . Map.keys $ Map.filter (== v) m
+
+
+------------------------------------------------------------------------------
+revLookup' :: Eq a => [(String, a)] -> Maybe a -> Maybe (String, a)
+revLookup' _ Nothing  = Nothing
+revLookup' m (Just v) = listToMaybe $ Prelude.filter ((== v) . snd) m
+
+
+------------------------------------------------------------------------------
+bootstrapButtonGroup :: forall t m a.(MonadWidget t m, Eq a, Show a)
+                     => Dynamic t [(a,String)]
+                     -> GWidget t m (Maybe a)
 bootstrapButtonGroup dynEntryList cfg = do
-  btns <- forDyn dynEntryList $ \pairs ->
-    Map.fromList (zip [1..] pairs)
+  btns :: Dynamic t (Map.Map Int a) <- forDyn dynEntryList $ \pairs ->
+    Map.fromList (zip [1..] (Prelude.map fst pairs))
+
+  -- let externalSets :: Event t (Maybe (String, a))
+  --     externalSets = attachWith (\m mayV -> revLookup' m mayV) (current dynEntryList) (_widgetConfig_setValue cfg)
 
   divAttrs <- forDyn (_widgetConfig_attributes cfg) $ \attrs ->
-    attrs <> "class" =: "btn-group"
-          <> "role" =: "group"
+    attrs <> "class"      =: "btn-group"
+          <> "role"       =: "group"
           <> "aria-label" =: "..."
-  groupWidget <- buttonGroup' handleOne btns (def {_widgetConfig_attributes = divAttrs})
-  mapWidget m groupWidget
+
+  groupWidget :: HtmlWidget t (Maybe a) <- 
+    buttonGroup' handleOne btns
+                                (WidgetConfig {_widgetConfig_attributes = divAttrs
+                                              ,_widgetConfig_setValue   = _widgetConfig_setValue cfg -- externalSets
+                                              ,_widgetConfig_initialValue = _widgetConfig_initialValue cfg }) -- revLookup' entryList (_widgetConfig_initialValue cfg)})
+
+  return groupWidget
+
+  -- groupWidget <- buttonGroup' handleOne btns (WidgetConfig {_widgetConfig_attributes = divAttrs
+  --                                                          ,_widgetConfig_setValue   = _widgetConfig_setVal cfg
+  --                                                          ,_widgetConfig_initialValue = _widgetConfig_initialValue cfg})
+  -- let x = groupWidget :: HtmlWidget t (Maybe a)
+  -- return groupWidget
+
+  -- mapWidget m groupWidget
 
   where
-    handleOne _ dynLabeledVal dynChecked = do
-      btnAttrs <- combineDyn btnAttrs dynLabeledVal dynChecked
-      (b,_) <- elDynAttr' "button" btnAttrs $ (dynText =<< mapDyn fst dynLabeledVal)
+    handleOne i dynV dynChecked = do
+      txt :: Dynamic t (Maybe String) <- combineDyn (\v m -> Prelude.lookup v m) dynV dynEntryList
+      txt' :: Dynamic t String <- mapDyn (fromMaybe "") txt
+      btnAttrs <- combineDyn btnAttrs dynV dynChecked
+      (b,_) <- elDynAttr' "button" btnAttrs $ (dynText txt')
       return (Click `domEvent` b)
 
-    btnAttrs (lbl, val) checked = "type" =: "button"
+    btnAttrs (val) checked = "type" =: "button"
                                <> "class" =: ("btn btn-default" <> bool "" " active" checked)
 
     m Nothing      = Nothing
@@ -112,6 +161,6 @@ demo = mainWidget $ do
   display (_hwidget_value res)
   res' <- radioGroup' "g" (constDyn [("Test",100), ("Test2", 200), ("Hello", 300)]) def
   display (_hwidget_value res')
-  res'' <- bootstrapButtonGroup (constDyn [("Test",100), ("Test2", 200), ("Hello", 300)]) def
+  res'' <- bootstrapButtonGroup (constDyn [(100,"Test"), (200,"Test2"), (300,"Hello")]) def
   display (_hwidget_value res'')
 
