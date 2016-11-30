c2d302b24f04778849431222aa5f87c322944c2d
Greg Hale <imalsogreg@gmail.com>
2016-11-29 21:28:13 -0500

    Improve synchronization across different routers by firing popState event

--- src/Reflex/Dom/Contrib/Router.hs
+++ src/Reflex/Dom/Contrib/Router.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleContexts         #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE JavaScriptFFI            #-}
+{-# LANGUAGE LambdaCase               #-}
 {-# LANGUAGE OverloadedStrings        #-}
 {-# LANGUAGE RankNTypes               #-}
 {-# LANGUAGE RecursiveDo              #-}
@@ -13,7 +14,8 @@
 module Reflex.Dom.Contrib.Router (
   -- == High-level routers
     webRoute
-  , textRoute
+  , partialPathRoute
+  , fullUriRoute
   , route
 
   -- == Router configuration
@@ -26,29 +28,28 @@ module Reflex.Dom.Contrib.Router (
   , routeConfig_forward
   , routeConfig_back
   , routeConfig_pushState
-  , routeConfig_pathBase
   , route_value
 
   ) where
 
 ------------------------------------------------------------------------------
 import           Control.Lens              (makeLenses)
+import           Control.Monad.Except      (ExceptT(..), lift, runExceptT)
 import           Control.Monad.IO.Class    (MonadIO, liftIO)
 import           Data.Bifunctor            (first)
-import           Data.Monoid ((<>))
+import           Data.Monoid               ((<>))
 import           Data.Default
-import           Data.Maybe                (fromJust)
 import qualified Data.Text                 as T
 import qualified Data.Text.Encoding        as T
+import           GHCJS.DOM.Types           (Location(..) )
 import           Reflex.Dom                hiding (EventName, Window)
 import qualified Web.Routes.PathInfo       as WR
 #if ghcjs_HOST_OS
--- import           Control.Monad.IO.Class    (liftIO)
 import qualified GHCJS.DOM                 as DOM
 import qualified GHCJS.DOM.Document        as DOM
 import           GHCJS.DOM.EventM          (on)
 import           GHCJS.DOM.History         (back, forward, pushState)
-import           GHCJS.DOM.Location        (getPathname)
+import           GHCJS.DOM.Location        (getPathname, toString)
 import           GHCJS.DOM.Window          (Window, getHistory,
                                             getLocation, popState)
 import           GHCJS.Marshal.Pure
@@ -61,25 +62,19 @@ data RouteConfig t a = RouteConfig
   { _routeConfig_forward   :: Event t () -- ^ Move the browser history forward
   , _routeConfig_back      :: Event t () -- ^ Move the browser history back
   , _routeConfig_pushState :: Event t a  -- ^ Push to the URL state
-  , _routeConfig_pathBase  :: T.Text
-    -- ^ The part of the URL not related to SPA routing
   }
 
 makeLenses ''RouteConfig
 
 instance Reflex t => Default (RouteConfig t a) where
-  def = RouteConfig never never never ""
+  def = RouteConfig never never never
 
 
 instance Reflex t => Monoid (RouteConfig t a) where
   mempty = def
-  mappend (RouteConfig f1 b1 p1 pb1) (RouteConfig f2 b2 p2 pb2) =
-    RouteConfig (mappend f1 f2) (mappend b1 b2) (leftmost [p1, p2]) pb12
-    where pb12 =
-            if pb1 `T.isPrefixOf` pb2 || pb2 `T.isPrefixOf` pb1
-            then (if T.length pb1 > T.length pb2 then pb1 else pb2)
-            else error
-                 "Tried to mappend RouteConfigs with conflicting base paths"
+  mappend (RouteConfig f1 b1 p1) (RouteConfig f2 b2 p2) =
+    RouteConfig (mappend f1 f2) (mappend b1 b2) (leftmost [p1, p2])
+
 
 data Route t a = Route {
     _route_value :: Dynamic t (Either T.Text a) -- ^ URL value
@@ -91,53 +86,71 @@ instance HasValue (Route t a) where
   type Value (Route t a) = Dynamic t (Either T.Text a)
   value = _route_value
 
--- | Manipulate and track the URL text for dynamic routing of a widget
+-- | Manipulate and track the URL 'GHCJS.DOM.Types.Location' for dynamic routing of a widget
 route
   :: (HasWebView m, MonadWidget t m)
-  => (T.Text -> Either T.Text a)
+  => (Location -> IO (Either T.Text a))
      -- ^ Decode the part of the path beyond '_routeConfig_pathBase' into an a
-  -> (a -> T.Text)
+  -> (a -> IO T.Text)
      -- ^ Encode the routing value
   -> RouteConfig t a
     -- ^ Routing widget configuration
   -> m (Route t a)
--- #if ghcjs_HOST_OS
-route to from (RouteConfig goForward goBack sSet pBase) = do
+route to from (RouteConfig goForward goBack sSet) = do
   win <- askDomWindow
-  path0 :: T.Text <- liftIO $ getLocation win >>= getPathname . fromJust
-  let pathVal0 = parsePath path0
+  locVal0 <- liftIO $ getLocation win >>= \case
+    Nothing -> return $ Left  "No window Location"
+    Just l  -> to l
   Just hist <- liftIO $ getHistory win
   performEvent_ $ ffor goForward $ \_ -> liftIO (forward hist)
   performEvent_ $ ffor goBack    $ \_ -> liftIO (back hist)
 
   _ <- performEvent $ ffor sSet $ \t -> liftIO $ do
-    pushState hist (pToJSVal (0 :: Int)) ("" :: T.Text) (pBase <> from t)
+    s <- from t
+    pushState hist (pToJSVal (0 :: Int)) ("" :: T.Text) s
+    dispatchEvent'
 
-  newLocs <- getPopState
-  let locVals = ffor newLocs $ \l ->
-        note "Bad path prefix" (T.stripPrefix pBase l) >>= to
-  Route <$> holdDyn pathVal0 (leftmost [Right <$> sSet, locVals])
+  locVals <- getPopState to
+  Route <$> holdDyn locVal0 (leftmost [Right <$> sSet, locVals])
 
-  where parsePath l = note "Bad path prefix" (T.stripPrefix pBase l) >>= to
 
 -------------------------------------------------------------------------------
 -- | Route a single page app according to any 'WR.PathInfo' a => a
 webRoute
   :: (MonadWidget t m, WR.PathInfo a)
-  => RouteConfig t a
+  => T.Text     -- ^ The part of the URL not related to SPA routing
+  -> RouteConfig t a
   -> m (Route t a)
-webRoute = route (first T.pack . WR.fromPathInfo . T.encodeUtf8)
-                 (WR.toPathInfo)
+webRoute pathBase = route decoder (return . (pathBase <>) . WR.toPathInfo)
+  where
+    decoder l = runExceptT $ do
+      pn <- lift $ getPathname l
+      ExceptT . return $ first T.pack . WR.fromPathInfo . T.encodeUtf8 =<<
+        note (pfxErr pn pathBase) (T.stripPrefix pathBase pn)
 
 
 -------------------------------------------------------------------------------
 -- | Route a single page app according to the part of the path after
 --   pathBase
-textRoute
+partialPathRoute
+  :: MonadWidget t m
+  => T.Text    -- ^ The part of the URL not related to SPA routing
+  -> RouteConfig t T.Text
+  -> m (Route t T.Text)
+partialPathRoute pathBase = route decoder (return . (pathBase <>))
+  where
+    decoder l = (\pn -> maybe (Left $ pfxErr pn pathBase)
+                              (Right . id) (T.stripPrefix pathBase pn)
+                ) <$> getPathname l
+
+
+-------------------------------------------------------------------------------
+-- | Route a single page app according to the full URI
+fullUriRoute
   :: MonadWidget t m
   => RouteConfig t T.Text
   -> m (Route t T.Text)
-textRoute = route Right id
+fullUriRoute = route (fmap Right . toString) return
 
 
 #if ghcjs_HOST_OS
@@ -154,23 +167,25 @@ askDomWindow = error "askDomWindow is only available to ghcjs"
 #endif
 
 
-getPopState :: (MonadWidget t m) => m (Event t T.Text)
--- #if ghcjs_HOST_OS
-getPopState = do
+getPopState :: (MonadWidget t m) => (Location -> IO (Either T.Text a)) -> m (Event t (Either T.Text a))
+getPopState decodeLocation = do
   window <- askDomWindow
   wrapDomEventMaybe window (`on` popState) $ do
-    l <- liftIO $ getLocation window
-    case l of
+    liftIO $ getLocation window >>= \case
       Nothing -> return Nothing
-      Just loc -> liftIO $ Just <$> getPathname loc
+      Just l  -> Just <$> decodeLocation l
 
 
 #if ghcjs_HOST_OS
+foreign import javascript unsafe "w = window; e = new PopStateEvent('popstate',{'view':window,'bubbles':true,'cancelable':true}); w.dispatchEvent(e);"
+  dispatchEvent' :: IO ()
 #else
-data Location
 data Window
 data JSVal
 data History
+
+dispatchEvent' :: IO ()
+dispatchEvent' = undefined
 
 forward :: History -> IO ()
 forward = undefined
@@ -202,6 +217,9 @@ on = undefined
 type EventM t e = ReaderT e IO
 data PopStateEvent
 data EventName t e
+
+toString :: Location -> IO T.Text
+toString = undefined
 #endif
 
 -------------------------------------------------------------------------------
@@ -209,3 +227,8 @@ data EventName t e
 note :: e -> Maybe a -> Either e a
 note _ (Just a) = Right a
 note e _        = Left e
+
+
+pfxErr :: T.Text -> T.Text -> T.Text
+pfxErr pn pathBase = "Encountered path (" <> pn
+            <> ") without expected prefix (" <> pathBase <> ")"
