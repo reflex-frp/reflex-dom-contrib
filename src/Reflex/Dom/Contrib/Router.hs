9b6862faccc1e71be903a10d18ee072f1712dea5
Greg Hale <imalsogreg@gmail.com>
2016-12-02 14:52:46 -0500

    Simplify route, add parameterized route', drop webroutes dep

--- src/Reflex/Dom/Contrib/Router.hs
+++ src/Reflex/Dom/Contrib/Router.hs
@@ -13,50 +13,42 @@
 
 module Reflex.Dom.Contrib.Router (
   -- == High-level routers
-    partialPathRoute
-  , fullUriRoute
-  , route
-
-  -- == Router configuration
-  , RouteConfig(..)
-
-  -- == Routing result
-  , Route(..)
-
-  , uriOrigin
-
-  -- == Lenses
-  , routeConfig_forward
-  , routeConfig_back
-  , routeConfig_pushState
-  , route_value
-  , route_fullURI
+    route
+  , route'
+  , partialPathRoute
 
   -- = Low-level URL bar access
-  , getLocation'
-  , getUrlText'
+  , getLoc
+  , getUrlText
+  , uriOrigin
+
+  -- = History movement
+  , goForward
+  , goBack
   ) where
 
 ------------------------------------------------------------------------------
-import           Control.Lens              (makeLenses)
-import           Control.Monad.Except      (ExceptT(..), lift, runExceptT)
+import           Control.Lens              ((&), (.~), (^.))
+-- import           Control.Monad             ((<=<))
+-- import           Control.Monad.Except      (ExceptT(..), lift, runExceptT)
 import           Control.Monad.IO.Class    (MonadIO, liftIO)
-import           Data.Bifunctor            (first)
-import           Data.Maybe                (fromMaybe)
+-- import           Data.Bifunctor            (first)
+import qualified Data.ByteString.Char8     as BS
+-- import           Data.Maybe                (fromJust, fromMaybe)
+import qualified Data.List                 as L
 import           Data.Monoid               ((<>))
-import           Data.Default
+-- import           Data.Default
 import qualified Data.Text                 as T
 import qualified Data.Text.Encoding        as T
 import           GHCJS.DOM.Types           (Location(..) )
 import           Reflex.Dom                hiding (EventName, Window)
 import qualified URI.ByteString            as U
-import qualified Web.Routes.PathInfo       as WR
 #if ghcjs_HOST_OS
 import qualified GHCJS.DOM                 as DOM
 import qualified GHCJS.DOM.Document        as DOM
 import           GHCJS.DOM.EventM          (on)
-import           GHCJS.DOM.History         (back, forward, pushState)
-import           GHCJS.DOM.Location        (getPathname, toString)
+import           GHCJS.DOM.History         (History, back, forward, pushState)
+import           GHCJS.DOM.Location        (toString)
 import           GHCJS.DOM.Window          (Window, getHistory,
                                             getLocation, popState)
 import           GHCJS.Marshal.Pure
@@ -64,125 +56,77 @@ import           GHCJS.Marshal.Pure
 import           Control.Monad.Reader      (ReaderT)
 #endif
 
-------------------------------------------------------------------------------
-data RouteConfig t a = RouteConfig
-  { _routeConfig_forward   :: Event t () -- ^ Move the browser history forward
-  , _routeConfig_back      :: Event t () -- ^ Move the browser history back
-  , _routeConfig_pushState :: Event t a  -- ^ Push to the URL state
-  }
 
-makeLenses ''RouteConfig
+type URI = U.URIRef U.Absolute
 
-instance Reflex t => Default (RouteConfig t a) where
-  def = RouteConfig never never never
-
-
-instance Reflex t => Monoid (RouteConfig t a) where
-  mempty = def
-  mappend (RouteConfig f1 b1 p1) (RouteConfig f2 b2 p2) =
-    RouteConfig (mappend f1 f2) (mappend b1 b2) (leftmost [p1, p2])
-  mconcat rcs =
-    RouteConfig (mconcat $ map _routeConfig_forward rcs)
-                (mconcat $ map _routeConfig_back rcs)
-                (leftmost $ map _routeConfig_pushState rcs)
-
-data Route t a = Route {
-    _route_value   :: Dynamic t (Either T.Text a)     -- ^ Routing value
-  , _route_fullURI :: Dynamic t (U.URIRef U.Absolute) -- ^ Full URI
-  }
-
-makeLenses ''Route
-
-instance HasValue (Route t a) where
-  type Value (Route t a) = Dynamic t (Either T.Text a)
-  value = _route_value
 
 -------------------------------------------------------------------------------
 -- | Manipulate and track the URL 'GHCJS.DOM.Types.Location' for dynamic
 --   routing of a widget
+--   These sources of URL-bar change will be reflected in the output URI
+--     - Input events to 'route'
+--     - Browser Forward/Back button clicks
+--     - forward/back javascript calls (or 'goForward'/'goBack') Haskell calls
+--     - Any URL changes followed by a popState event
+--   But external calls to pushState that don't manually fire a popState
+--   won't be detected
 route
   :: (HasWebView m, MonadWidget t m)
-  => (Location -> IO (Either T.Text a))
-     -- ^ Decode the part of the path beyond '_routeConfig_pathBase' into an a
-  -> (a -> IO T.Text)
-     -- ^ Encode the routing value to a Text that will be passed to `pushState`
-  -> RouteConfig t a
-    -- ^ Routing widget configuration
-  -> m (Route t a)
-route to from (RouteConfig goForward goBack sSet) = do
-  win     <- askDomWindow
-  loc0    <- liftIO $ windowURI win
-  locVal0 <- liftIO $ to =<< fmap (fromMaybe (error "No location"))
-                                  (getLocation win)
+  => Event t T.Text
+  -> m (Dynamic t (U.URIRef U.Absolute))
+route pushTo = do
+  loc0    <- getURI
+  _ <- performEvent $ ffor pushTo $ \t -> do
+    withHistory $ \h -> pushState h (pToJSVal (0 :: Int)) ("" :: T.Text) t
+    liftIO dispatchEvent'
 
-  Just hist <- liftIO $ getHistory win
-  performEvent_ $ ffor goForward $ \_ -> liftIO (forward hist)
-  performEvent_ $ ffor goBack    $ \_ -> liftIO (back hist)
+  locUpdates <- getPopState
 
-  _ <- performEvent $ ffor sSet $ \t -> liftIO $ do
-    s <- from t
-    pushState hist (pToJSVal (0 :: Int)) ("" :: T.Text) s
-    dispatchEvent'
+  -- Route <$> holdDyn locVal0 (leftmost [sSet, locUpdates]) <*> holdDyn loc0 locs
+  -- Because we trigger a popState manually, there is no more need to use the
+  -- sSet events to update the dynamic uri
+  -- TODO make sure this actually works on all the expected URL bar update sources
+  holdDyn loc0 locUpdates
 
-  locVals <- getPopState to
-  locs <- performEvent $ ffor locVals $ \_ -> liftIO (windowURI win)
-
-  Route <$> holdDyn locVal0 (leftmost [Right <$> sSet, locVals]) <*> holdDyn loc0 locs
-
-
-windowURI :: Window -> IO (U.URIRef U.Absolute)
-windowURI w = do
-  l <- fromMaybe (error "Window has no location") <$> getLocation w
-  either (error "No parse of window location") id . U.parseURI U.laxURIParserOptions . T.encodeUtf8 <$> toString l
-
--------------------------------------------------------------------------------
--- | Route a single page app according to any 'WR.PathInfo' a => a
-webRoute
-  :: (MonadWidget t m, WR.PathInfo a)
-  => T.Text     -- ^ The part of the URL not related to SPA routing
-  -> RouteConfig t a
-  -> m (Route t a)
-webRoute pathBase = route decoder (return . (pathBase <>) . WR.toPathInfo)
-  where
-    decoder l = runExceptT $ do
-      pn <- lift $ getPathname l
-      ExceptT . return $ first T.pack . WR.fromPathInfo . T.encodeUtf8 =<<
-        note (pfxErr pn pathBase) (T.stripPrefix pathBase pn)
+route'
+  :: forall t m a b. MonadWidget t m
+  => (URI -> a -> URI)
+  -> (URI -> b)
+  -> Event t a
+  -> m (Dynamic t b)
+route' encode decode routeUpdate = do
+  rec rUri <- route (T.decodeUtf8 . U.serializeURIRef' <$> urlUpdates)
+      let urlUpdates = attachWith encode (current rUri) routeUpdate
+  return $ decode <$> rUri
 
 
 -------------------------------------------------------------------------------
 -- | Route a single page app according to the part of the path after
 --   pathBase
 partialPathRoute
-  :: MonadWidget t m
-  => T.Text    -- ^ The part of the URL not related to SPA routing
-  -> RouteConfig t T.Text
-  -> m (Route t T.Text)
-partialPathRoute pathBase = route decoder (return . (pathBase <>))
+  :: forall t m. MonadWidget t m
+  => [T.Text]  -- ^ The path segments not related to SPA routing
+  -> Event t [T.Text] -- ^ Updates to the path segments used for routing
+  -> m (Dynamic t [T.Text]) -- ^ Path segments used for routing
+partialPathRoute pathBase pathUpdates = do
+  route' (flip updateUrl) parseParts pathUpdates
   where
-    decoder l = (\pn -> maybe (Left $ pfxErr pn pathBase)
-                              (Right . id) (T.stripPrefix pathBase pn)
-                ) <$> getPathname l
+
+    toPath :: [T.Text] -> BS.ByteString
+    toPath parts =
+      "/" <> BS.intercalate "/" (T.encodeUtf8 <$> (pathBase <> parts))
+
+    updateUrl :: [T.Text] -> URI -> URI
+    updateUrl updateParts u = u & U.pathL .~ toPath updateParts
+
+    parseParts :: URI -> [T.Text]
+    parseParts u =
+      maybe (error . T.unpack $ pfxErr u pathBase) (map T.decodeUtf8) .
+      L.stripPrefix (T.encodeUtf8 <$> pathBase) .
+      BS.split '/' . BS.drop 1 $ u ^. U.pathL
 
 
 -------------------------------------------------------------------------------
--- | Route a single page app according to the full URI
---   The dynamic returned to you will be a Text of the full URI
---   The route string you pass in will be forwarded as-is to
---   pushState.
-fullUriRoute
-  :: MonadWidget t m
-  => RouteConfig t (U.URIRef U.Absolute)
-  -> m (Route t (U.URIRef U.Absolute))
-fullUriRoute cfg = do
-  route uriDecoder (return . T.decodeUtf8 . U.serializeURIRef') cfg
-    where
-      uriDecoder = fmap (first (T.pack . show) .
-                         U.parseURI U.laxURIParserOptions .
-                         T.encodeUtf8) .
-                   toString
-
-
 uriOrigin :: U.URIRef U.Absolute -> T.Text
 uriOrigin r = T.decodeUtf8 $ U.serializeURIRef' r'
   where
@@ -191,6 +135,8 @@ uriOrigin r = T.decodeUtf8 $ U.serializeURIRef' r'
            , U.uriFragment = mempty
            }
 
+
+-------------------------------------------------------------------------------
 #if ghcjs_HOST_OS
 -- | Get the DOM window object.
 askDomWindow :: (HasWebView m, MonadIO m) => m Window
@@ -205,30 +151,57 @@ askDomWindow = error "askDomWindow is only available to ghcjs"
 #endif
 
 
-getPopState :: (MonadWidget t m) => (Location -> IO (Either T.Text a)) -> m (Event t (Either T.Text a))
-getPopState decodeLocation = do
+-------------------------------------------------------------------------------
+getPopState :: (MonadWidget t m) => m (Event t URI)
+getPopState = do
   window <- askDomWindow
-  wrapDomEventMaybe window (`on` popState) $ do
-    liftIO $ getLocation window >>= \case
-      Nothing -> return Nothing
-      Just l  -> Just <$> decodeLocation l
+  wrapDomEventMaybe window (`on` popState) $ liftIO $ do
+    Just loc <- getLocation window
+    locStr <- toString loc
+    return . hush $ U.parseURI U.laxURIParserOptions (T.encodeUtf8 locStr)
+
+
+-------------------------------------------------------------------------------
+goForward :: (HasWebView m, MonadIO m) => m ()
+goForward = withHistory forward
+
+
+-------------------------------------------------------------------------------
+goBack :: (HasWebView m, MonadIO m) => m ()
+goBack = withHistory back
+
+
+-------------------------------------------------------------------------------
+withHistory :: (HasWebView m, MonadIO m) => (History -> IO a) -> m a
+withHistory act = do
+  Just h <- liftIO . getHistory =<< askDomWindow
+  liftIO $ act h
+
 
 
 -------------------------------------------------------------------------------
 -- | (Unsafely) get the 'GHCJS.DOM.Location.Location' of a window
-getLocation' :: (HasWebView m, MonadIO m) => m Location
+getLoc :: (HasWebView m, MonadIO m) => m Location
 #if ghcjs_HOST_OS
-getLocation' = fromMaybe (error "Window has no Location") <$>
-  (askDomWindow >>= liftIO . getLocation)
+getLoc = do
+  Just win <- liftIO . getLocation =<< askDomWindow
+  return win
 #else
-getLocation' = error "getLocation' is only available to ghcjs"
+getLoc = error "getLocation' is only available to ghcjs"
 #endif
 
 
 -------------------------------------------------------------------------------
 -- | (Unsafely) get the URL text of a window
-getUrlText' :: (HasWebView m, MonadIO m) => m T.Text
-getUrlText' = getLocation' >>= liftIO . toString
+getUrlText :: (HasWebView m, MonadIO m) => m T.Text
+getUrlText = getLoc >>= liftIO . toString
+
+
+getURI :: (HasWebView m, MonadIO m) => m URI
+getURI = do
+  l <- getUrlText
+  return $ either (error "No parse of window location") id .
+    U.parseURI U.laxURIParserOptions $ T.encodeUtf8 l
 
 
 #if ghcjs_HOST_OS
@@ -251,9 +224,6 @@ back = undefined
 getLocation :: Window -> IO (Maybe Location)
 getLocation = undefined
 
-getPathname :: Location -> IO T.Text
-getPathname = undefined
-
 getHistory :: Window -> IO (Maybe History)
 getHistory = undefined
 
@@ -275,15 +245,14 @@ data EventName t e
 
 toString :: Location -> IO T.Text
 toString = undefined
+
 #endif
 
--------------------------------------------------------------------------------
--- | Helper functions
-note :: e -> Maybe a -> Either e a
-note _ (Just a) = Right a
-note e _        = Left e
 
+hush :: Either e a -> Maybe a
+hush (Right a) = Just a
+hush _ = Nothing
 
-pfxErr :: T.Text -> T.Text -> T.Text
-pfxErr pn pathBase = "Encountered path (" <> pn
-            <> ") without expected prefix (" <> pathBase <> ")"
+pfxErr :: URI -> [T.Text] -> T.Text
+pfxErr pn pathBase = "Encountered path (" <> T.decodeUtf8 (U.serializeURIRef' pn)
+            <> ") without expected prefix (" <> T.intercalate "/" pathBase <> ")"
