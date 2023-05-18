cb1211f0afb9581d1f27f9b86526d8409b560ce7
lazyLambda <galen.sprout@gmail.com>
2023-05-18 12:38:57 -0400

    remove HasJSContext

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-{ rpRef ? "4b4a2c8679d6f3042180d2974b31c43bfc506b91"
-, rpSha ? "185i8ccqk0xc05ckd921fs7nnaj88gihmlazny1dk004hxq78cj2"
+{ rpRef ? "ae415044d5ef35d947f0689a66f349ae9e2b7aee"
+, rpSha ? "0mpipci4mxpaqfn2iqc6gwh0n11rvp1l2fz7g703gp12djr8srfd"
 }:
 
-let rp = builtins.fetchTarball {
-  url = "https://github.com/reflex-frp/reflex-platform/archive/${rpRef}.tar.gz";
-  sha256 = rpSha;
+let
+  rp = builtins.fetchTarball {
+    url = "https://github.com/reflex-frp/reflex-platform/archive/${rpRef}.tar.gz";
+    sha256 = rpSha;
 };
 
 in
@@ -15,14 +16,16 @@ in
         rev = "4f2d85f2f1aa4c6bff2d9fcfd3caad443f35476e";
         sha256 = "1vzfi3i3fpl8wqs1yq95jzdi6cpaby80n8xwnwa8h2jvcw3j7kdz";
       }) {};
+      doJailbreak = pkgs.haskell.lib.doJailbreak;
   in
   {
     name = "reflex-dom-contrib";
     overrides = self: super: with pkgs.haskell.lib;
-       {
-       };
+      {
+        reflex-dom-contrib = doJailbreak (super.reflex-dom-contrib); 
+      };
     packages = {
-      reflex-dom-contrib = gitignore.gitignoreSource [] ./.;
+      reflex-dom-contrib = (gitignore.gitignoreSource [] ./.);
     };
     shellToolOverrides = ghc: super: {
       ghcid = pkgs.haskellPackages.ghcid;
